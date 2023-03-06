6c317c1effaa95815c1354cbadb4857ac90d2f6a
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-03-06 13:37:39 +0000

    Iteration 1

--- .ccls-cache/@home@runner@quizProjectGombos/main.c
+++ .ccls-cache/@home@runner@quizProjectGombos/main.c
@@ -6,6 +6,18 @@
 #include <stdio.h>
 
 int main(void) {
+  int correctAnswers;
+  correctAnswers = 0;
+  char answer;
   
+  printf("What continent is Canada in?\na: North America\nb: South America\nc: Africa\nd: Europe\ne: Asia\nf: Oceania\n");
+  scanf("%c", &answer);
+  if (answer == 'a') {
+    printf("You are correct!\n");
+    correctAnswers += 1;
+  }
+  else {
+    printf("You are incorrect, the correct answer was:\n\ta: North America\n");
+  }
   return 0;
 }
