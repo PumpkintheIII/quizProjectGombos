f12577f07cf80f017e5bb1dd3813b039f72494ff
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-03-06 14:04:00 +0000

    Iteration 2

--- main.c
+++ main.c
@@ -7,11 +7,12 @@
 
 int main(void) {
   int correctAnswers;
+  float grade;
   correctAnswers = 0;
   char answer;
   
-  printf("What continent is Canada in?\na: North America\nb: South America\nc: Africa\nd: Europe\ne: Asia\nf: Oceania\n");
-  scanf("%c", &answer);
+  printf("1: What continent is Canada in?\na: North America\nb: South America\nc: Africa\nd: Europe\ne: Asia\nf: Oceania\n");
+  scanf("%s", &answer);
   if (answer == 'a') {
     printf("You are correct!\n");
     correctAnswers += 1;
@@ -19,5 +20,22 @@ int main(void) {
   else {
     printf("You are incorrect, the correct answer was:\n\ta: North America\n");
   }
+  printf("2: What continent is Belgium in?\na: North America\nb: South America\nc: Africa\nd: Europe\ne: Asia\nf: Oceania\n");
+  scanf("%s", &answer);
+  if (answer == 'd') {
+    printf("You are correct!\n");
+    correctAnswers += 1;
+  }
+  else {
+    printf("You are incorrect, the correct answer was:\n\td: Europe\n");
+  }
+  
+
+
+
+  grade = correctAnswers/2;
+  grade = grade * 100;
+  printf("\nYou got %d out of 2 correct for a grade of %.02f%%", correctAnswers, grade);
+  
   return 0;
 }
